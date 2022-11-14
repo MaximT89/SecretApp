814eecd7a948c0ef6eb434e486c10dc74d9620ba
BuenasDias <tmaksim1989@gmail.com>
2022-11-14 09:42:28 +0300

    change status bar color bg and texts

--- app/src/main/java/com/second/world/secretapp/ui/screens/MainActivity.kt
+++ app/src/main/java/com/second/world/secretapp/ui/screens/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.second.world.secretapp.ui.screens
 
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.second.world.secretapp.R
 import com.second.world.secretapp.core.bases.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,5 +12,10 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//
+//        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//        window.statusBarColor = ContextCompat.getColor(this, R.color.gr)
     }
 }
